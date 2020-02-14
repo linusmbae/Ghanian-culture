78f43e55d95c7b1acf1310a59ed7762b1947b7a5
Linus Mutugi <linusmutugi5178@gmail.com>
2020-02-14 15:49:46 +0300

    add date validity checker

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
-function submit() {
-  var day=parseInt(document.getElementById("Day").value);
-
-if (day<0 && day>30)
-{
-    document.getElementById("Day").innerText = "INVALID INPUT";
-}
 
 
+function myFunction() {
+  let showday=document.getElementById("siku");
+  let showmonth=document.getElementById("Month");
+  let showcc=document.getElementById("cc");
+  let showyear=document.getElementById("year");
+  let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  if (!showday.checkValidity())
+   {
+     document.getElementById("dvalidity").innerHTML = "rectify your date";
+     alert("invalid date");
+   } else if (!showmonth.checkValidity())
+   {
+     document.getElementById("mvalidity").innerHTML = "rectify your date";
+     alert("invalid date");
+   }
+   else {
+     document.getElementById("dvalidity").innerHTML = "";
+     document.getElementById("mvalidity").innerHTML = "";
+   };
 
 
-}
+
+};
