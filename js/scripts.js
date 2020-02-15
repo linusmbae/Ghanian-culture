0bc11c26eaa8624b409587a4d34c978cc150bab1
Linus Mutugi <linusmutugi5178@gmail.com>
2020-02-15 11:24:04 +0300

    add class col-md-4,form-control, modify form, remove class row,remove class submit

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,25 @@
 function myFunction() {
   let showday=document.getElementById("siku");
   let showmonth=document.getElementById("Month");
-  let showcc=document.getElementById("cc");
   let showyear=document.getElementById("year");
   let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   let males = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
   let females = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-  if (!showday.checkValidity())
+  if (!showday.checkValidity() && !showmonth.checkValidity())
    {
      document.getElementById("dvalidity").innerHTML = "rectify your date";
+     document.getElementById("mvalidity").innerHTML = "rectify your date";
      alert("invalid date");
-   } else if (!showmonth.checkValidity())
+   }else if (!showday.checkValidity())
+   {
+     document.getElementById("dvalidity").innerHTML = "rectify your date";
+      document.getElementById("mvalidity").innerHTML = "";
+     alert("invalid date");
+   }else if (!showmonth.checkValidity())
    {
      document.getElementById("mvalidity").innerHTML = "rectify your date";
+      document.getElementById("dvalidity").innerHTML = "";
      alert("invalid date");
    }
    else {
